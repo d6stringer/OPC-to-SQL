052d16a07b2aadeb89cf448810276d006cb500ec
Daniel Woodson <s.dwoodson@gmail.com>
2021-11-23 15:46:46 -0800

    yip

--- main.py
+++ main.py
@@ -3,14 +3,40 @@
 #Started on: 8/25/2021
 #Purpose: To create a bridge between the OPC server(s) of the custom BM equipment to the postgreSQL relational database
 
-#import postgreSQL_Connector
-import OPC_Connector
+import postgreSQL_Connector as pSQL
+import time
 
-def print_distance():
-    print(OPC_Connector.get_distance())
+import OPC_Connector
+import utilities
+
+yams = utilities.yaml_loader('opc_config.yml')
+
 
 if __name__ == '__main__':
-    print_distance()
+
+    ls_data = [yams['nodes']['job_number'],
+               yams['nodes']['count_of_blobs'],
+               yams['nodes']['part_width'],
+               yams['nodes']['histogram_avg'],
+               yams['nodes']['histogram_contrast']
+               ]
+
+    value = 0
+    try:
+        while True:
+            new_value = OPC_Connector.get_node_value(yams['nodes']['part_count'])
+            if value != new_value:
+                value = new_value
+                data = OPC_Connector.get_node_values(ls_data)
+                data.insert(0,value)
+                job_status = OPC_Connector.get_node_value(yams['nodes']['job_active'])
+                if job_status:
+                    print(data)
+                    pSQL.send_linescan_to_pSQL(data)
+                    #OPC_Connector.write_ok(yams['nodes']['write_ok'])
+
+            time.sleep(0.1)
 
 
-
+    except KeyboardInterrupt:
+        print('break')
