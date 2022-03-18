37d4372fc00ab136e2f1d5f65e230a291c335d4b
Daniel Woodson <s.dwoodson@gmail.com>
2022-03-18 15:17:36 -0700

    Added a try except to the postgres connector to try to catch an error

--- postgreSQL_Connector.py
+++ postgreSQL_Connector.py
@@ -17,13 +17,17 @@ def pSQL_conn():
     return pSQL_conn
 
 def send_data_to_pSQL(data):
-    connection = pSQL_conn()
-    cursor = connection.cursor()
-    squirrls = """
-        INSERT INTO machine_run_data (id, traveler_id, traveler_code, part_count, total_part_count, time_stamp, top_heater_SV, top_heater_PV, top_heater_enable, bottom_heater_SV, bottom_heater_PV, bottom_heater_enable, cooling_fans, line_speed, waste_tension, feed_tension, roll_diameter, start_mode, slow_start_mode, stop_mode, jog_mode, safety_ok, system_enable, waste_tension_enable, feed_tension_enable) 
-        VALUES (default, 1,%s, %s, %s,current_timestamp, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-        """
-    cursor.execute(squirrls, data)
-    connection.commit()
-    connection.close()
-    return
+    try:
+        connection = pSQL_conn()
+        cursor = connection.cursor()
+        squirrls = """
+            INSERT INTO machine_run_data (id, traveler_id, traveler_code, part_count, total_part_count, time_stamp, top_heater_SV, top_heater_PV, top_heater_enable, bottom_heater_SV, bottom_heater_PV, bottom_heater_enable, cooling_fans, line_speed, waste_tension, feed_tension, roll_diameter, start_mode, slow_start_mode, stop_mode, jog_mode, safety_ok, system_enable, waste_tension_enable, feed_tension_enable) 
+            VALUES (default, 1,%s, %s, %s,current_timestamp, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """
+        cursor.execute(squirrls, data)
+        connection.commit()
+        connection.close()
+        return
+    except IndexError:
+        print('This is the index error')
+        return
